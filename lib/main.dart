6bee10b245dcec87207f69e247b9db5645fc1e51
Sutinan_Sriviset <sutinansriviset@gmail.com>
2025-08-29 23:07:37 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+
+runApp(const MyApp());
   runApp(const ChildrenApp());
 }
 
